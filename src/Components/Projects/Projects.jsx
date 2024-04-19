0542a734e41f4261c2fd4beeeb4781f208c1a00c
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-04-19 15:01:47 +0200

    add responsiveness to projects section

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,10 +1,17 @@
 import "./Projects.scss";
 //import projectList from "../..projects.json";
-import projects from "../../assets/projects.json";
+//import projects from "../../assets/projects.json";
 import SingleProjectCard from "../SingleProjectCard/SingleProjectCard";
-//import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    fetch(`/projects.json`)
+      .then((response) => response.json())
+      .then((data) => setProjects(data))
+      .catch((error) => console.error(error));
+  }, []);
   return (
     <main className="projects">
       <h1 className="projects--title">Mes Projets</h1>
--- src/Components/SingleProjectCard/SingleProjectCard.jsx
+++ src/Components/SingleProjectCard/SingleProjectCard.jsx
@@ -3,28 +3,29 @@ import PropTypes from "prop-types";
 import { FaGithub } from "react-icons/fa6";
 
 function SingleProjectCard({ project }) {
-  console.log("test");
-  console.log("inside card", project);
+  console.log(project.secondPic);
+
   return (
-    <article className="projects__singleProject">
+    <article className={`singleProject ${project.flex}`}>
       <img
-        className="projects__singleProject--thumbnail"
+        className={`${project.direction}`}
         src={`${project.img}`}
         alt={project.alt}
       />
-      {project.img2 && (
-        <img
-          className="projects__singleProject--thumbnail"
-          src={`${project.img2}`}
-          alt={project.alt2}
-        />
-      )}
-      <div className="projects__singleProject--description">
+      <img
+        className={`${project.direction}`}
+        src={`${project.secondPic}`}
+        alt={project.alt2}
+      />
+
+      <div className="singleProject__description">
         <h2>{project.title}</h2>
-        <p>{project.short}</p>
-        <p>{project.description}</p>
-        <button className="projects__singleProject--description--button">
-          Voir le code <FaGithub className="projects--socialIcons" />
+        <p className="singleProject__description--paragraph">{project.short}</p>
+        <p className="singleProject__description--paragraph">
+          {project.description}
+        </p>
+        <button className="singleProject--description--button">
+          Voir le code <FaGithub className="singleProject--socialIcons" />
         </button>
       </div>
     </article>
@@ -37,9 +38,11 @@ SingleProjectCard.propTypes = {
     short: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     img: PropTypes.string.isRequired,
-    img2: PropTypes.string,
+    secondPic: PropTypes.string,
+    direction: PropTypes.string.isRequired,
     alt: PropTypes.string.isRequired,
     alt2: PropTypes.string,
+    flex: PropTypes.string.isRequired,
   }).isRequired,
 };
 
